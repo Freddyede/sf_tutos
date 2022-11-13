4bdfa5c400339ddc785f3a4ceaf16cfe1487cd64
Freddyede <f.patouillard@it-students.fr>
2022-11-13 13:55:48 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\{
+    Routing\Annotation\Route,
+    HttpFoundation\Response
+};
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="app_home")
+     */
+    public function index(): Response {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+}
